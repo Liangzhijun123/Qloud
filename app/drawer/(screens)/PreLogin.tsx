0a06d027c1d2a7a10794f0deb59699779bb391c4
zhi_jun <liangzhijun1216@gmail.com>
2025-03-28 16:40:18 -0400

    feat: styling in prelogin. added images

--- app/drawer/(screens)/PreLogin.tsx
+++ app/drawer/(screens)/PreLogin.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { useRouter } from 'expo-router';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { useRouter } from "expo-router";
+import { Dimensions } from "react-native";
 
 const PreLogin = ({ navigation }: { navigation: any }) => {
   const [currentScreen, setCurrentScreen] = useState(0);
@@ -10,7 +11,10 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
     {
       content: (
         <View style={styles.contentContainer}>
-          <Image source={{ uri: 'https://via.placeholder.com/300' }} style={styles.image} />
+          <Image
+            source={require("../../../assets/loginImage.png")}
+            style={styles.image}
+          />
           <Text style={styles.logo}>QLOUD</Text>
         </View>
       ),
@@ -18,12 +22,24 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
     {
       content: (
         <View style={styles.contentContainer}>
-          <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+          <Image
+            source={require("../../../assets/prelogin1.png")}
+            style={styles.image}
+          />
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Text>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.joinButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.joinButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Join Now</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.signInButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.signInButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Sign In</Text>
             </TouchableOpacity>
           </View>
@@ -33,12 +49,24 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
     {
       content: (
         <View style={styles.contentContainer}>
-          <Text style={styles.text}>Suspendisse varius enim in eros elementum tristique.</Text>
+          <Image
+            source={require("../../../assets/prelogin2.png")}
+            style={styles.image}
+          />
+          <Text style={styles.text}>
+            Suspendisse varius enim in eros elementum tristique.
+          </Text>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.joinButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.joinButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Join Now</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.signInButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.signInButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Sign In</Text>
             </TouchableOpacity>
           </View>
@@ -48,12 +76,24 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
     {
       content: (
         <View style={styles.contentContainer}>
-          <Text style={styles.text}>Curabitur gravida arcu ac tortor dignissim convallis.</Text>
+          <Image
+            source={require("../../../assets/prelogin3.png")}
+            style={styles.image}
+          />
+          <Text style={styles.text}>
+            Curabitur gravida arcu ac tortor dignissim convallis.
+          </Text>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.joinButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.joinButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Join Now</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.signInButton} onPress={() => navigation.navigate('Login')} >
+            <TouchableOpacity
+              style={styles.signInButton}
+              onPress={() => navigation.navigate("Login")}
+            >
               <Text style={styles.buttonText}>Sign In</Text>
             </TouchableOpacity>
           </View>
@@ -68,7 +108,9 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
       <View style={styles.dotsContainer}>
         {screens.map((_, index) => (
           <TouchableOpacity key={index} onPress={() => setCurrentScreen(index)}>
-            <View style={[styles.dot, currentScreen === index && styles.activeDot]} />
+            <View
+              style={[styles.dot, currentScreen === index && styles.activeDot]}
+            />
           </TouchableOpacity>
         ))}
       </View>
@@ -76,64 +118,77 @@ const PreLogin = ({ navigation }: { navigation: any }) => {
   );
 };
 
+const screenWidth = Dimensions.get("window").width;
+const aspectRatio = 16 / 9;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   contentContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    marginBottom: 20,
+    position: "relative",
   },
   image: {
-    width: 300,
-    height: 300,
-    marginBottom: 20,
+    width: screenWidth,
+    height: 900,
+    resizeMode: "cover",
+    zIndex: 1,
   },
   logo: {
     fontSize: 48,
-    fontWeight: 'bold',
-    color: '#000',
+    fontWeight: "bold",
+    color: "#000",
+    paddingTop: 150,
+    marginBottom: 20,
+    zIndex: 2,
+    position: "absolute",
   },
   text: {
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 30,
-    color: '#333',
+    color: "#333",
   },
   buttonContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
   },
   joinButton: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     padding: 12,
     borderRadius: 8,
   },
   signInButton: {
-    backgroundColor: '#555',
+    backgroundColor: "#555",
     padding: 12,
     borderRadius: 8,
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
   dotsContainer: {
-    flexDirection: 'row',
-    marginTop: 40,
+    flexDirection: "row",
+    marginTop: 300,
+    position: "absolute",
+    
   },
   dot: {
     width: 12,
     height: 12,
     borderRadius: 6,
-    backgroundColor: '#ddd',
+    backgroundColor: "#ddd",
     marginHorizontal: 5,
   },
   activeDot: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
   },
 });
 
